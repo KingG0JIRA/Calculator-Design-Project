c25728a5ec72ac932c46d5c542ee5fc6025f2c4a
VasuAnnan <appuabhishekas2004@gmail.com>
2025-03-02 23:52:37 +0530

    Update

--- Calculator/JS/index.js
+++ Calculator/JS/index.js
@@ -96,3 +96,12 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+} 
